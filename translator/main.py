67901a896dfdfcb2d79514d7d383eddb5ce0cc93
Дмитрий Демидов <dmitriidemidov@Air-Dmitrij-2.Dlink>
2020-11-22 22:54:09 +0300

    Auth logic for translator is implemented.

--- translator/main.py
+++ translator/main.py
@@ -4,4 +4,11 @@ URL_AUTH: str = 'https://developers.lingvolive.com/api/v1.1/authenticate'
 URL_TRANSLATE: str = 'https://developers.lingvolive.com/api/v1/Minicard'
 KEY: str = 'Y2Y2YmYyZGItMmIyMS00MGY2LWIzNzItOWUwM2M4ZjY2NjBkOjEyMDE4NWE3NmJjYzRhMjdhYzEwOTk4Nzg4NTQ3ZGFm'
 
+headers_auth = {'Authorization': 'Basic ' + KEY}
+
+auth = requests.post(URL_AUTH, headers=headers_auth)
+
+print(auth.status_code)
+print(auth.text)
+
 print("Hello translator!")
