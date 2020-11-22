24dfb4520bbca12db310515cce1e2c422278c26e
Дмитрий Демидов <dmitriidemidov@Air-Dmitrij-2.Dlink>
2020-11-22 22:26:24 +0300

    Translator main file is added.

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ print(1 + 2 + a + 2 * b)
 
 if (a > b) & (1 == 1): print('a > b'); print('I love Python')
 print('I love footbal')
+# comment
---
+++ translator/main.py
@@ -0,0 +1,7 @@
+import requests
+
+URL_AUTH: str = 'https://developers.lingvolive.com/api/v1.1/authenticate'
+URL_TRANSLATE: str = 'https://developers.lingvolive.com/api/v1/Minicard'
+KEY: str = 'Y2Y2YmYyZGItMmIyMS00MGY2LWIzNzItOWUwM2M4ZjY2NjBkOjEyMDE4NWE3NmJjYzRhMjdhYzEwOTk4Nzg4NTQ3ZGFm'
+
+print("Hello translator!")
