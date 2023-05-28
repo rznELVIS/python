d3359785645df393aa4de80f8b84c87ad7fcd4aa
dmitriidemidov <dmitri-demidiv@yandex.ru>
2023-05-28 22:35:36 +0300

    Validation of params is added.

--- codingTasks/PalindromeSearch/palindromeSearcher.py
+++ codingTasks/PalindromeSearch/palindromeSearcher.py
@@ -1,2 +1,6 @@
 class PalindromeSearcher:
-	pass
+	def search(self, text: str) -> str:
+		if not text or text.isspace():
+			raise ValueError("text doesn't have value.")
+
+		return ""
--- codingTasks/PalindromeSearch/palindromeSearcherTests.py
+++ codingTasks/PalindromeSearch/palindromeSearcherTests.py
@@ -1,2 +1,46 @@
-class PalindromeSearcherTests:
-	pass
+import unittest
+from palindromeSearcher import PalindromeSearcher
+
+class PalindromeSearcherTests(unittest.TestCase):
+	def test_search_text_is_set_string_is_returned(self):
+		# arrange
+		searcher: PalindromeSearcher = PalindromeSearcher()
+
+		# act
+		result: str = searcher.search("text")
+
+		# assert
+		self.assertTrue(isinstance(result, str))
+
+	def test_search_text_is_empty_string_raise_value_error(self):
+		# arrange
+		searcher: PalindromeSearcher = PalindromeSearcher()
+
+		# act && assert
+		with self.assertRaises(ValueError) as context:
+			searcher.search("")
+
+		# assert
+		self.assertEqual("text doesn't have value.", str(context.exception))
+
+	def test_search_text_is_none_string_raise_value_error(self):
+		# arrange
+		searcher: PalindromeSearcher = PalindromeSearcher()
+
+		# act && assert
+		with self.assertRaises(ValueError) as context:
+			searcher.search(None)
+
+		# assert
+		self.assertEqual("text doesn't have value.", str(context.exception))
+
+	def test_search_text_is_whitespace_string_raise_value_error(self):
+		# arrange
+		searcher: PalindromeSearcher = PalindromeSearcher()
+
+		# act && assert
+		with self.assertRaises(ValueError) as context:
+			searcher.search("   ")
+
+		# assert
+		self.assertEqual("text doesn't have value.", str(context.exception))
