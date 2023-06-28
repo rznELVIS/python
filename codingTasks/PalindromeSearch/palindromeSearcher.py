802e49ecf91b9e9ed5e8cf8118f4161f6e3869d9
Dmitry Demidov <Dmitry_Demidov@epam.com>
2023-06-28 16:53:40 +0300

    2nd try to fix palindrome

--- codingTasks/PalindromeSearch/palindromeSearcher.py
+++ codingTasks/PalindromeSearch/palindromeSearcher.py
@@ -3,16 +3,29 @@ class PalindromeSearcher:
 		if not text or text.isspace():
 			raise ValueError("text doesn't have value.")
 
-		for i in range(0, len(text)):
-			result: str = self.__search(text, i)
+
+		result: str = self.__search(text, 0)
 
 		return ""
 
 	def __search(self, text: str, start_index: int) -> str:
-		result: str = ""
+		palindrome: str = ""
 
+		end_index_decrement: int = 0
 		for index in range(start_index, len(text)):
-			result = result + text[index]
+			end_index = len(text) - end_index_decrement - 1
 
-		return result
+			start_char: str = text[index]
+			end_char: str = text[end_index]
+			if start_char == end_char:
+				palindrome = palindrome + text[index]
+			else:
+				new_palindrome: str = self.__search(text, index)
+
+				if len(new_palindrome) > len(palindrome):
+					palindrome = new_palindrome
+
+			end_index_decrement += 1
+
+		return palindrome
 
